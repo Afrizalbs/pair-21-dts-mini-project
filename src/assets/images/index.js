650dc258e64ccb4f02c602eddbf594281bde6433
Afrizal <afrizalbagas.03@gmail.com>
2022-07-18 09:29:29 +0700

    Add New Assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import LogoApp from "./LogoApp.png";
+import BgLogin from "./BgLogin.png";
+import DummyProfile from "./DummyProfile.png";
+import DummyProfile2 from "./DummyProfile2.png";
+import DummyProfile3 from "./DummyProfile3.png";
+import DummyProfile4 from "./DummyProfile4.png";
+
+export {
+  LogoApp,
+  BgLogin,
+  DummyProfile,
+  DummyProfile2,
+  DummyProfile3,
+  DummyProfile4,
+};
